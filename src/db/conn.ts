8b039ea8102bea705687ddbd65cfd5eea0078ff8
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-10 14:39:30 -0300

    feat: add MongoDB connection setup in conn.ts

---
+++ src/db/conn.ts
@@ -0,0 +1,13 @@
+import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        await connect(process.env.MONGO_URI as string);
+        console.log('MongoDbConectado');
+    } catch (error) {
+        console.log('Erro ao conectar ao MongoDB', error);
+    }
+}
