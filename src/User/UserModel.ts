f77c437a621ebbfffc97342f8fc35ffd662b88bf
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2025-04-10 14:41:07 -0300

    feat: create Use Model and role enum

---
+++ src/User/UserModel.ts
@@ -0,0 +1,16 @@
+import { model, Schema } from "mongoose"
+import { userRole } from "../utils/enum/userRole";
+
+interface IUser {
+    email: string,
+    password: string,
+    role: userRole
+}
+
+const userSchema = new Schema<IUser>({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: Number, enum: Object.values(userRole), default: userRole.regular }
+})
+
+export const User = model<IUser>('User', userSchema);
